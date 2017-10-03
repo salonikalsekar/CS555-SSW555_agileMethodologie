a19162da3b834389b492a23439f491c5ce111919
Sravanthi Kanchi <shravik593@gmail.com>
2017-10-03 19:35:33 -0400

    userStory08.py
    User Story 08 - Birth After Marriage of Parents

---
+++ userStory08.py
@@ -0,0 +1,142 @@
+import datetime 
+
+def converttoDate(date):
+    n = date.split()
+    if(n[1] == 'JAN'): n[1] = '01';
+    if(n[1] == 'FEB'): n[1] = '02';
+    if(n[1] == 'MAR'): n[1] = '03';
+    if(n[1] == 'APR'): n[1] = '04';
+    if(n[1] == 'MAY'): n[1] = '05';
+    if(n[1] == 'JUN'): n[1] = '06';
+    if(n[1] == 'JUL'): n[1] = '07';
+    if(n[1] == 'AUG'): n[1] = '08';
+    if(n[1] == 'SEP'): n[1] = '09';
+    if(n[1] == 'OCT'): n[1] = '10';
+    if(n[1] == 'NOV'): n[1] = '11';
+    if(n[1] == 'DEC'): n[1] = '12';
+    if(n[2] in ['1', '2', '3', '4', '5', '6', '7', '8', '9']):
+        n[2] = '0' + n[2]
+    return (n[0] + '-' + n[1] + '-' + n[2])
+
+
+def birthAfterMarriageofParents(list_indi, list_fam):
+    list1 = []
+    for m in list_fam:
+        if(m[5] != []):
+            start_date = m[3]
+            for ti in m[5]:
+                if(m[4] != 0):
+                    end_date = add9Months(m[4])
+                    if(getBirthDateByID(list_indi, ti) >= end_date):
+                        list1.append(j)
+                        print("Individual " + ti + " in family " + m[0] + " was born after 9 months after the divorce of his/her parents.")
+                        continue
+                if(getBirthDateByID(list_indi, ti) <= start_date):
+                    list1.append(ti)
+                    print("Individual " + ti + " in family " + m[0] + " was born before the marriage of his/her parents.")
+                    continue
+                
+    if(len(list1)==0):
+        print(" Children having birth dates afer marriage of parents")
+        print()
+    else:
+        print("List of individuals who were born nine months after the divorce of their parents or  born before  marriage  ", end = '')
+        print(list1)
+        print()
+
+
+def getBirthDateByID(list_indi, id):
+    for j in list_indi:
+        if(j[0] == id):
+            return j[3]
+
+
+def getCurrDate():
+    c_date = str(datetime.date.today())
+    return c_date
+
+    return op_list
+
+def fam_list():
+    o_list = [0 for i in range(6)]
+    o_list[5] = []
+    return o_list
+
+def ind_list():
+    o_list = [0 for i in range(7)]
+    o_list[5] = []
+    return o_list
+
+def getLastName(s):
+    t=''
+    for i in s:
+        if(i != '/'):
+            t += i
+    return t
+
+
+
+
+def parse(file):
+    indi_flag = 0
+    fam_flag = 0
+    list_indi = []
+    list_fam = []
+    indi = ind_list()
+    fam = fam_list()
+    for line in file:
+        str = line.split()
+        if(str != []):
+            if(str[0] == '0'):
+                if(indi_flag == 1):
+                    list_indi.append(indi)
+                    indi = ind_list()
+                    indi_flag = 0
+                if(fam_flag == 1):
+                    list_fam.append(fam)
+                    fam = fam_list()
+                    fam_flag = 0
+                if(str[1] in ['NOTE', 'HEAD', 'TRLR']):
+                    pass
+                else:
+                    if(str[2] == 'INDI'):
+                        indi_flag = 1
+                        indi[0] = (str[1])
+                    if(str[2] == 'FAM'):
+                        fam_flag = 1
+                        fam[0] = (str[1])
+            if(str[0] == '1'):
+                if(str[1] == 'NAME'):
+                    indi[1] = str[2] + " " + getLastName(str[3])
+                if(str[1] == 'SEX'):
+                    indi[2] = str[2]
+                if(str[1] in ['BIRT', 'DEAT', 'MARR', 'DIV']):
+                    date_id = str[1]
+                if(str[1] == 'FAMS'):
+                    indi[5].append(str[2])
+                if(str[1] == 'FAMC'):
+                    indi[6] = str[2]
+                if(str[1] == 'HUSB'):
+                    fam[1] = str[2]
+                if(str[1] == 'WIFE'):
+                    fam[2] = str[2]
+                if(str[1] == 'CHIL'):
+                    fam[5].append(str[2])
+            if(str[0] == '2'):
+                if(str[1] == 'DATE'):
+                    date = str[4] + " " + str[3] + " " + str[2]
+                    if(date_id == 'BIRT'):
+                        indi[3] = converttoDate(date)
+                    if(date_id == 'DEAT'):
+                        indi[4] = converttoDate(date)
+                    if(date_id == 'MARR'):
+                        fam[3] = converttoDate(date)
+                    if(date_id == 'DIV'):
+                        fam[4] = converttoDate(date)
+    return list_indi, list_fam
+
+def main(file):
+    list_indi, list_fam = parse(file)
+    birthAfterMarriageofParents(list_indi, list_fam)
+main('My-Family_Sravanthi_KanchiSSW555.ged')
+
