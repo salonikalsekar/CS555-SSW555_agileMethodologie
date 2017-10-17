8a58221cb91918525f30992e0285b848ece2f74c
Akanksha Chatra <achatra@stevens.edu>
2017-10-17 18:45:08 -0400

    User Story 10

---
+++ userStory10.py
@@ -0,0 +1,136 @@
+import datetime
+
+def individualList():
+    output_list = [0 for i in range(7)]
+    output_list[5] = []
+    return output_list
+def getLastName(str):
+    lastName=''
+    for i in str:
+        if(i != '/'):
+            lastName += i
+    return lastName
+
+def familyList():
+    output_list = [0 for i in range(6)]
+    output_list[5] = []
+    return output_list
+        
+def getMarriageByID(list_individual, id, dateOfMarriage):
+    marriageDate = dateOfMarriage.split('-')
+    yearOfMarriage = int(marriageDate[0])
+    monthOfMarriage = int(marriageDate[1])
+    dateOfMarriage = int(marriageDate[2])
+    for i in list_individual:
+        if(i[0] == id):
+            dateOfBirth = i[3]
+    birthDate = dateOfBirth.split('-')
+    yearOfBirth = int(birthDate[0])
+    monthOfBirth = int(birthDate[1])
+    dateOfBirth = int(birthDate[2])
+    return yearOfMarriage - yearOfBirth - ((monthOfMarriage, dateOfMarriage) < (monthOfBirth, dateOfBirth))
+
+def dateFormatConversion(date):
+    m = date.split()
+    if(m[1] == 'JAN'): m[1] = '01';
+    if(m[1] == 'FEB'): m[1] = '02';
+    if(m[1] == 'MAR'): m[1] = '03';
+    if(m[1] == 'APR'): m[1] = '04';
+    if(m[1] == 'MAY'): m[1] = '05';
+    if(m[1] == 'JUN'): m[1] = '06';
+    if(m[1] == 'JUL'): m[1] = '07';
+    if(m[1] == 'AUG'): m[1] = '08';
+    if(m[1] == 'SEP'): m[1] = '09';
+    if(m[1] == 'OCT'): m[1] = '10';
+    if(m[1] == 'NOV'): m[1] = '11';
+    if(m[1] == 'DEC'): m[1] = '12';
+    if(m[2] in ['1', '2', '3', '4', '5', '6', '7', '8', '9']):
+        m[2] = '0' + m[2]
+    return (m[0] + '-' + m[1] + '-' + m[2])
+
+def marriageAfter14(list_individual, list_family):
+    marriageAfter_list = []
+    for i in list_family:
+        if(getMarriageByID(list_individual, i[1], i[3])<14):
+            marriageAfter_list.append(i[1])
+            print("User Story 10: This individual with ID " + i[1] + " married before he/she turned 14 in family with ID" + i[0])
+        if(getMarriageByID(list_individual, i[2], i[3])<14):
+            marriageAfter_list.append(i[2])
+            print("User Story 10: This individual with ID " + i[2] + " married before he/she turned 14 in family with ID " + i[0])
+
+    if(len(marriageAfter_list) != 0):
+        print("User Story 10: The individuals mentioned below married before they turned 14: ")
+        print(marriageAfter_list)       
+    else:
+        print("User Story 10: There are no individuals who married before they turned 14 years.")
+        
+
+def toParse(gedFileName):
+    f = open(gedFileName,'r')
+    list_individual = []
+    list_family = []
+    indi_on = 0
+    fam_on = 0
+    individual = individualList()
+    family = familyList()
+    for line in f:
+        str = line.split()
+        if(str != []):
+            if(str[0] == '0'):
+                if(fam_on == 1):
+                    list_family.append(family)
+                    family = familyList()
+                    fam_on = 0
+                if(indi_on == 1):
+                    list_individual.append(individual)
+                    individual = individualList()
+                    indi_on = 0               
+                if(str[1] in ['NOTE', 'HEAD', 'TRLR']):
+                    pass
+                else:
+                    if(str[2] == 'INDI'):
+                        indi_on = 1
+                        individual[0] = (str[1])
+                    if(str[2] == 'FAM'):
+                        fam_on = 1
+                        family[0] = (str[1])
+            if(str[0] == '1'):
+                if(str[1] == 'NAME'):
+                    individual[1] = str[2] + " " + getLastName(str[3])
+                if(str[1] == 'SEX'):
+                    individual[2] = str[2]
+                if(str[1] == 'FAMS'):
+                    individual[5].append(str[2])
+                if(str[1] == 'FAMC'):
+                    individual[6] = str[2]
+                if(str[1] == 'HUSB'):
+                    family[1] = str[2]
+                if(str[1] == 'WIFE'):
+                    family[2] = str[2]
+                if(str[1] == 'CHIL'):
+                    family[5].append(str[2])
+                if(str[1] in ['BIRT', 'DEAT', 'MARR', 'DIV']):
+                    date_id = str[1]                                
+            if(str[0] == '2'):
+                if(str[1] == 'DATE'):
+                    date = str[4] + " " + str[3] + " " + str[2]
+                    if(date_id == 'MARR'):
+                        family[3] = dateFormatConversion(date)
+                    if(date_id == 'DIV'):
+                        family[4] = dateFormatConversion(date)
+                    if(date_id == 'BIRT'):
+                        individual[3] = dateFormatConversion(date)
+                    if(date_id == 'DEAT'):
+                        individual[4] = dateFormatConversion(date)
+                    
+    return list_individual,list_family
+
+
+
+def main(gedFileName):
+    list_individual, list_family= toParse(gedFileName)
+    list_individual.sort()
+    list_family.sort()
+    marriageAfter14(list_individual, list_family)
+
+main('D:/SSW-555/Week 5/Akanksha_Homework 4/Homework 4/akankshaGedcom.ged')
